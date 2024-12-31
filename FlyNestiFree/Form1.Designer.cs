ed98cdc548f052d1948f4581c2e2c90da9fc4fd5
HartashikALT <flaystar.space@gmail.com>
2024-12-31 19:56:52 +0700

    Added script editor

--- FlyNestiFree/Form1.Designer.cs
+++ FlyNestiFree/Form1.Designer.cs
@@ -28,22 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.title = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Label();
+            this.minimize = new System.Windows.Forms.Label();
+            this.scriptTextBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(0, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(800, 35);
+            this.title.TabIndex = 0;
+            this.title.Text = "FlyNesty by HqHacks";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.title.MouseDown += new System.Windows.Forms.MouseEventHandler(this.title_mouseDown);
+            this.title.MouseMove += new System.Windows.Forms.MouseEventHandler(this.title_mouseMove);
+            this.title.MouseUp += new System.Windows.Forms.MouseEventHandler(this.title_mouseUp);
+            // 
+            // close
+            // 
+            this.close.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.ForeColor = System.Drawing.Color.Gray;
+            this.close.Location = new System.Drawing.Point(764, 0);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(24, 35);
+            this.close.TabIndex = 1;
+            this.close.Text = "X";
+            this.close.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // minimize
+            // 
+            this.minimize.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimize.ForeColor = System.Drawing.Color.White;
+            this.minimize.Location = new System.Drawing.Point(730, 0);
+            this.minimize.Name = "minimize";
+            this.minimize.Size = new System.Drawing.Size(28, 35);
+            this.minimize.TabIndex = 2;
+            this.minimize.Text = "M";
+            this.minimize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.minimize.Click += new System.EventHandler(this.minimize_Click);
+            // 
+            // scriptTextBox
+            // 
+            this.scriptTextBox.AcceptsTab = true;
+            this.scriptTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.scriptTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.scriptTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scriptTextBox.ForeColor = System.Drawing.Color.White;
+            this.scriptTextBox.Location = new System.Drawing.Point(12, 38);
+            this.scriptTextBox.Name = "scriptTextBox";
+            this.scriptTextBox.Size = new System.Drawing.Size(630, 340);
+            this.scriptTextBox.TabIndex = 4;
+            this.scriptTextBox.Text = "-- Welcome to the FlyNesty Script Loader\n-- Our site - flynesty.space\n-- Thanks f" +
+    "or using our application";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scriptTextBox);
+            this.Controls.Add(this.minimize);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.title);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FlyNesty Free - Roblox Executor";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label close;
+        private System.Windows.Forms.Label minimize;
+        private System.Windows.Forms.RichTextBox scriptTextBox;
     }
 }
 
--- FlyNestiFree/Form1.cs
+++ FlyNestiFree/Form1.cs
@@ -13,6 +13,10 @@ namespace FlyNestiFree
 {
     public partial class Form1 : Form
     {
+        private bool isDragging = false;
+        private Point startPoint = new Point(0, 0);
+
+
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
         (
@@ -29,6 +33,42 @@ namespace FlyNestiFree
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 5, 5));
+        }
+
+        private void title_mouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                startPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void title_mouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - startPoint.X, p.Y - startPoint.Y);
+            }
+        }
+
+        private void title_mouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.isDragging = false;
+            }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
