560b8b4061be20ac526cfe8e4e392c2b05a90635
HartashikALT <flaystar.space@gmail.com>
2025-01-01 02:48:12 +0700

    updated design

--- FlyNestiFree/Form1.Designer.cs
+++ FlyNestiFree/Form1.Designer.cs
@@ -32,6 +32,12 @@
             this.close = new System.Windows.Forms.Label();
             this.minimize = new System.Windows.Forms.Label();
             this.scriptTextBox = new System.Windows.Forms.RichTextBox();
+            this.attach = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.inject = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            this.filesList = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // title
@@ -82,17 +88,117 @@
             this.scriptTextBox.ForeColor = System.Drawing.Color.White;
             this.scriptTextBox.Location = new System.Drawing.Point(12, 38);
             this.scriptTextBox.Name = "scriptTextBox";
-            this.scriptTextBox.Size = new System.Drawing.Size(630, 340);
+            this.scriptTextBox.Size = new System.Drawing.Size(630, 336);
             this.scriptTextBox.TabIndex = 4;
             this.scriptTextBox.Text = "-- Welcome to the FlyNesty Script Loader\n-- Our site - flynesty.space\n-- Thanks f" +
-    "or using our application";
+    "or using our application\nprint(\"Hello World!\")";
+            // 
+            // attach
+            // 
+            this.attach.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.attach.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.attach.FlatAppearance.BorderSize = 0;
+            this.attach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.attach.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.attach.ForeColor = System.Drawing.Color.White;
+            this.attach.Location = new System.Drawing.Point(12, 380);
+            this.attach.Name = "attach";
+            this.attach.Size = new System.Drawing.Size(133, 44);
+            this.attach.TabIndex = 5;
+            this.attach.Text = "Attach";
+            this.attach.UseVisualStyleBackColor = false;
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.clear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.clear.FlatAppearance.BorderSize = 0;
+            this.clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clear.ForeColor = System.Drawing.Color.White;
+            this.clear.Location = new System.Drawing.Point(151, 380);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(133, 44);
+            this.clear.TabIndex = 6;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.save.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.save.FlatAppearance.BorderSize = 0;
+            this.save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.save.ForeColor = System.Drawing.Color.White;
+            this.save.Location = new System.Drawing.Point(290, 380);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(133, 44);
+            this.save.TabIndex = 7;
+            this.save.Text = "Save As";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // inject
+            // 
+            this.inject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.inject.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.inject.FlatAppearance.BorderSize = 0;
+            this.inject.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inject.ForeColor = System.Drawing.Color.White;
+            this.inject.Location = new System.Drawing.Point(509, 380);
+            this.inject.Name = "inject";
+            this.inject.Size = new System.Drawing.Size(133, 44);
+            this.inject.TabIndex = 8;
+            this.inject.Text = "Inject";
+            this.inject.UseVisualStyleBackColor = false;
+            // 
+            // refresh
+            // 
+            this.refresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.refresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.refresh.FlatAppearance.BorderSize = 0;
+            this.refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.refresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.refresh.ForeColor = System.Drawing.Color.White;
+            this.refresh.Location = new System.Drawing.Point(648, 380);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(140, 44);
+            this.refresh.TabIndex = 9;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = false;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // filesList
+            // 
+            this.filesList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.filesList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.filesList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.filesList.ForeColor = System.Drawing.Color.White;
+            this.filesList.FormattingEnabled = true;
+            this.filesList.ItemHeight = 16;
+            this.filesList.Items.AddRange(new object[] {
+            "welcome.txt"});
+            this.filesList.Location = new System.Drawing.Point(648, 38);
+            this.filesList.Name = "filesList";
+            this.filesList.Size = new System.Drawing.Size(140, 336);
+            this.filesList.TabIndex = 10;
+            this.filesList.SelectedIndexChanged += new System.EventHandler(this.filesList_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 435);
+            this.Controls.Add(this.filesList);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.inject);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.attach);
             this.Controls.Add(this.scriptTextBox);
             this.Controls.Add(this.minimize);
             this.Controls.Add(this.close);
@@ -111,6 +217,12 @@
         private System.Windows.Forms.Label close;
         private System.Windows.Forms.Label minimize;
         private System.Windows.Forms.RichTextBox scriptTextBox;
+        private System.Windows.Forms.Button attach;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button inject;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.ListBox filesList;
     }
 }
 
--- FlyNestiFree/Form1.cs
+++ FlyNestiFree/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace FlyNestiFree
 {
@@ -16,6 +17,7 @@ namespace FlyNestiFree
         private bool isDragging = false;
         private Point startPoint = new Point(0, 0);
 
+        private String scriptsFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Scripts"); 
 
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
@@ -33,6 +35,21 @@ namespace FlyNestiFree
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 5, 5));
+            loadFileList();
+        }
+
+        private void loadFileList()
+        {
+            if (!Directory.Exists(scriptsFolder))
+            {
+                Directory.CreateDirectory(scriptsFolder);
+            }
+
+            filesList.Items.Clear();
+            var files = Directory.GetFiles(scriptsFolder, "*.*")
+                .Where(f => f.EndsWith(".txt") || f.EndsWith(".lua"))
+                .Select(Path.GetFileName);
+            filesList.Items.AddRange(files.ToArray());
         }
 
         private void title_mouseDown(object sender, MouseEventArgs e)
@@ -69,6 +86,55 @@ namespace FlyNestiFree
         private void minimize_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            this.scriptTextBox.Clear();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            loadFileList();
+        }
+
+        private void filesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (filesList.SelectedItem != null)
+            {
+                String filePath = Path.Combine(scriptsFolder, filesList.SelectedItem.ToString());
+                try
+                {
+                    scriptTextBox.Text = File.ReadAllText(filePath);
+                } 
+                catch(Exception ex)
+                {
+                    MessageBox.Show($"Internal error while reading file: {ex.Message}");
+                }
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.InitialDirectory = scriptsFolder;
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt|Lua Files (*.lua)|*.lua|All Files (*.*)|*.*";
+                
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, scriptTextBox.Text);
+                        MessageBox.Show("File successfully saved!");
+                        loadFileList();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Internal error while saving file: {ex.Message}");
+                    }
+                }
+            }
         }
     }
 }
